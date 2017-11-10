6cfe433de55a1ad62589302c62bf30cb55698a24
yaomenghao <your_email@youremail.com584977959@qq.com>
2017-11-10 20:54:08 +0800

    ‘stumansys’

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView= findViewById(R.id.text);
+
+
+
+
+        textView.setText("124");
+    }
+}
